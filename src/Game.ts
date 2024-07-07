1a9d2aa5bdab1012c9b0525894f49e467766bfa5
Old-Butt-Gold <andrey2004andrey2021@gmail.com>
2024-07-07 22:34:24 +0300

    fixed click after end of the game

--- src/Game.ts
+++ src/Game.ts
@@ -9,6 +9,8 @@ export class Game {
     private readonly gameContainer: HTMLDivElement;
     private isFirstClick = true;
 
+    private stateEndOfGame : string | null = null;
+
     private readonly difficulties = {
         easy: { size: 10, mines: 10 },
         medium: { size: 16, mines: 40 },
@@ -26,6 +28,7 @@ export class Game {
     }
 
     public init() {
+        this.stateEndOfGame = null;
         this.isFirstClick = true;
         const { size, mines } = this.difficulties[this.difficultySelect.value];
         this.board = new Board(size, mines, this.handleCellClick.bind(this), this.handleCellRightClick.bind(this));
@@ -34,6 +37,10 @@ export class Game {
     }
 
     private handleCellClick(row: number, col: number) {
+        if (this.stateEndOfGame !== null) {
+            return;
+        }
+
         if (this.isFirstClick) {
             while (this.board.getCell(row, col).mine) {
                 const { size, mines } = this.difficulties[this.difficultySelect.value];
@@ -59,6 +66,10 @@ export class Game {
     }
 
     private handleCellRightClick(row: number, col: number) {
+        if (this.stateEndOfGame !== null) {
+            return;
+        }
+
         const cell = this.board.getCell(row, col);
         if (cell.revealed) return;
 
@@ -69,6 +80,7 @@ export class Game {
     private gameOver() {
         this.timer.stop();
         alert('Game Over!');
+        this.stateEndOfGame = 'lost';
         this.board.revealAllMines();
         this.board.renderBoard(this.gameContainer);
     }
@@ -78,6 +90,7 @@ export class Game {
         const mineCount = this.board.getMineCount();
         if (unrevealedCells === mineCount) {
             this.timer.stop();
+            this.stateEndOfGame = 'won';
             alert('You win!');
         }
     }
